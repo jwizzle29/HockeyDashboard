d4f5a98cbcb82c161cf262c67da97d332d59c927
jwizzle29 <joshgg29@gmail.com>
2021-08-23 03:00:22 -0400

    added PROD_URL

--- index.php
+++ index.php
@@ -1,5 +1,13 @@
 <?php
 require_once('vendor/autoload.php');
+define("PROD_URL", "http://{$_SERVER['SERVER_NAME']}/HockeyDashboard");
 $controller = new App\Api\Controller();
 $controller->sayHi($_SERVER['REQUEST_URI']);
+echo "<br>";
 echo "ooohahatfdsft";
+?>
+<li><a href="<?php echo PROD_URL ?>/Home">Home</a></li>
+<li><a href="<?php echo PROD_URL ?>/Sales">Sales</a></li>
+<li><a href="<?php echo PROD_URL ?>/Tickets">Tickets</a></li>
+<li><a href="<?php echo PROD_URL ?>/Data">Data</a></li>
+
